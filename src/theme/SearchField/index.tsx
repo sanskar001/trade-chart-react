1743fe61a9df3d454b916801475bc13308c7e2ea
dev3 <dev3@ungrammary.com>
2023-05-24 19:29:31 +0530

    WIP: add search field component

--- src/theme/SearchField/index.tsx
+++ src/theme/SearchField/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import SearchIcon from "../../components/SVG/SearchIcon";
 import { formatClass } from "../../util/formatClass";
 
