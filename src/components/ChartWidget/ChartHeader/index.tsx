5bb4b070b18ae5adafb00a078b79da7fec27156c
dev3 <dev3@ungrammary.com>
2023-05-24 19:28:16 +0530

    WIP: add search field component

--- src/components/ChartWidget/ChartHeader/index.tsx
+++ src/components/ChartWidget/ChartHeader/index.tsx
@@ -1,29 +1,10 @@
-import React, { useState } from "react";
-import Modal from "@/theme/Modal";
+import React from "react";
 
 const ChartHeader: React.FC = () => {
-  const [showModal, setShowModal] = useState<boolean>(false);
-
   return (
     <div className="border-b border-linen-orange">
       <div className="h-[40px] flex items-center p-[2px]">
-        <h6 onClick={setShowModal.bind(null, true)}>Chart Header</h6>
-        <Modal
-          isShowModal={showModal}
-          onClose={setShowModal.bind(null, false)}
-          modalClass="bg-green-100"
-          headerText="Header"
-          footerText="Hello Modal footer"
-          backdropClass="bg-blue-500/10"
-        >
-          <div>
-            <h1>Modal</h1>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Molestias, labore.
-            </p>
-          </div>
-        </Modal>
+        <h6>Chart Header</h6>
       </div>
     </div>
   );
---
+++ src/theme/SearchField/index.tsx
@@ -0,0 +1,43 @@
+import React, { useMemo } from "react";
+import SearchIcon from "../../components/SVG/SearchIcon";
+import { formatClass } from "../../util/formatClass";
+
+interface SearchFieldProps {
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  className?: string;
+  style?: React.CSSProperties;
+  autoFocus?: boolean;
+  disabled?: boolean;
+  placeholder?: string;
+}
+
+const SearchField: React.FC<SearchFieldProps> = ({
+  value,
+  onChange,
+  className = "",
+  style,
+  autoFocus = false,
+  disabled = false,
+  placeholder = "Search",
+}) => {
+  return (
+    <div className={formatClass(`search-field ${className}`)} style={style}>
+      <label htmlFor="search">
+        <SearchIcon />
+      </label>
+      <input
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        autoComplete="off"
+        autoFocus={autoFocus}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
+
+export default SearchField;
