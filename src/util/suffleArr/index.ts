d912784273c616bf8bd5eac12cc1e3cde09274aa
dev3 <dev3@ungrammary.com>
2023-07-04 12:11:10 +0530

    REFACTOR: add suffleArray util function

--- src/util/suffleArr/index.ts
+++ src/util/suffleArr/index.ts
@@ -2,11 +2,12 @@ function getRandomIndex(max: number) {
   return Math.trunc(Math.random() * max);
 }
 
-type SuffleArrType<T> = (_arr: Array<T>) => Array<T>;
+type SuffleArrType<T> = (_arr: Array<T>, _times?: number) => Array<T>;
 
-export const suffleArr: SuffleArrType<any> = (arr) => {
+export const suffleArr: SuffleArrType<any> = (arr, times) => {
   const len = arr.length;
-  for (let i = 0; i < len; i++) {
+  const freq = times || len;
+  for (let i = 0; i < freq; i++) {
     const firstIndex = getRandomIndex(len);
     const secondIndex = getRandomIndex(len);
     [arr[firstIndex], arr[secondIndex]] = [arr[secondIndex], arr[firstIndex]];
