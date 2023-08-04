c7a0bd9293ca26b9a50cabf53f19c3ddea59ed8d
dev3 <dev3@ungrammary.com>
2023-08-04 20:32:31 +0530

    FEAT: add parse chart data in chart service

--- src/services/chart/chart.service.ts
+++ src/services/chart/chart.service.ts
@@ -9,13 +9,60 @@ import {
   volumePriceScaleOption,
   volumeSeriesOption,
 } from "./chart-config";
-import { CandleList, ChartContainer } from "./type";
+import {
+  CandleList,
+  ChartContainer,
+  ChartType,
+  GetAreaData,
+  GetCandleStickData,
+  GetLineData,
+  SeriesApi,
+} from "./type";
+
+const getCandleStickData: GetCandleStickData = (data) => {
+  const seriesData = data.map((candle) => ({
+    open: candle.open,
+    high: candle.high,
+    low: candle.low,
+    close: candle.close,
+    time: candle.time,
+  }));
+  return seriesData;
+};
+
+const getLineData: GetLineData = (data) => {
+  const seriesData = data.map((candle) => ({
+    value: candle.close,
+    time: candle.time,
+  }));
+  return seriesData;
+};
+
+const getAreaData: GetAreaData = (data) => {
+  const seriesData = data.map((candle) => ({
+    value: candle.close,
+    time: candle.time,
+  }));
+  return seriesData;
+};
+
+const chartData: {
+  // eslint-disable-next-line
+  [key in ChartType]: GetCandleStickData | GetLineData | GetAreaData;
+} = {
+  candle: getCandleStickData,
+  line: getLineData,
+  area: getAreaData,
+};
 
 export class TradeChart {
   private readonly instance: IChartApi;
+  private activeSeries: SeriesApi;
 
-  constructor(container: ChartContainer) {
+  constructor(container: ChartContainer, chartType?: ChartType) {
     this.instance = createChart(container, chartOptions);
+
+    this.activeSeries = this.createSeries(chartType || "candle");
 
     window.addEventListener("resize", () => {
       if (container) {
@@ -31,7 +78,7 @@ export class TradeChart {
     return volumeSeries;
   }
 
-  private createCandleSeries() {
+  private createCandleStickSeries() {
     const candleStickSeries = this.instance.addCandlestickSeries(
       candleStickSeriesOption
     );
@@ -54,6 +101,51 @@ export class TradeChart {
     return areaSeries;
   }
 
+  private createSeries(chartType: ChartType) {
+    // eslint-disable-next-line
+    const chartSeries: { [key in ChartType]: SeriesApi } = {
+      candle: this.createCandleStickSeries(),
+      line: this.createLineSeries(),
+      area: this.createAreaSeries(),
+    };
+    return chartSeries[chartType];
+  }
+
+  /*
+  private setCandleStickSeriesData(data: CandleList) {
+    const candleStickSeriesData = data.map((candle) => {
+      return {
+        open: candle.open,
+        high: candle.high,
+        low: candle.low,
+        close: candle.close,
+        time: candle.time,
+      };
+    });
+    this.createVolumeSeries().setData(candleStickSeriesData);
+  }
+
+  private setLineSeriesData(data: CandleList) {
+    const lineSeriesData = data.map((candle) => {
+      return {
+        value: candle.close,
+        time: candle.time,
+      };
+    });
+    this.createVolumeSeries().setData(lineSeriesData);
+  }
+
+  private setAreaSeriesData(data: CandleList) {
+    const areaSeriesData = data.map((candle) => {
+      return {
+        value: candle.close,
+        time: candle.time,
+      };
+    });
+    this.createVolumeSeries().setData(areaSeriesData);
+  }
+*/
+
   public setVolumeSeriesData(data: CandleList) {
     const volumeSeriesData = data.map((candle) => {
       return {
@@ -63,5 +155,11 @@ export class TradeChart {
       };
     });
     this.createVolumeSeries().setData(volumeSeriesData);
+  }
+
+  public loadChart(chartType: ChartType, data: CandleList) {
+    this.instance.removeSeries(this.activeSeries);
+    this.activeSeries = this.createSeries(chartType);
+    this.activeSeries.setData(chartData[chartType](data));
   }
 }
--- src/services/chart/type.ts
+++ src/services/chart/type.ts
@@ -1,14 +1,32 @@
-import { Time } from "lightweight-charts";
+import { ISeriesApi, Time } from "lightweight-charts";
 
 type Price = number;
-export interface Candle {
+
+export interface CandleStick {
   time: Time;
   low: Price;
   high: Price;
   open: Price;
   close: Price;
+}
+
+export interface Point {
+  time: Time;
+  value: Price;
+}
+
+export interface Candle extends CandleStick {
   volume?: Price;
 }
 
 export type CandleList = Array<Candle>;
 export type ChartContainer = string | HTMLElement;
+export type ChartType = "candle" | "line" | "area";
+export type SeriesApi = ISeriesApi<"Area" | "Line" | "Candlestick">;
+
+// eslint-disable-next-line
+export type GetCandleStickData = (data: CandleList) => CandleStick[];
+// eslint-disable-next-line
+export type GetLineData = (data: CandleList) => Point[];
+// eslint-disable-next-line
+export type GetAreaData = (data: CandleList) => Point[];
